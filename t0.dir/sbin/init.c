8864a6aae68663db8c522b941d0dbc8acf954d8a
nguyjer <nguyjer@gmail.com>
2024-04-29 18:57:12 -0500

    done

--- t0.dir/sbin/init.c
+++ t0.dir/sbin/init.c
@@ -11,10 +11,12 @@ int main(int argc, char **argv)
 {
 
     
-    int fd = open("/data/d4vdstereo.wav", 0);
+    int fd = open("/data/stereo.wav", 0);
     play_audio(fd);
-    seek(fd, 0);
+    close(fd);
+    fd = open("/data/d4vdstereo.wav", 0);
     play_audio(fd);
+    close(fd);
 
     printf("Exited sys call.\n");
     
